f12d946c6402015c05899fa1514b3e391b86ad46
ginjardev <arthurshur@gmail.com>
2023-03-17 02:51:01 +0000

    json tutorial

--- json/json_tut.py
+++ json/json_tut.py
@@ -1,43 +1,37 @@
 import json
 import requests
 
-#mother python dict
+# mother python dict
 mother = {
-     "name": "Asake Babatunde",
-     "age": 28,
-     "marital status": "Married",
-     "children": ["Ayo", "Tolu", "Simi"],
-     "staff": False,
-     "next of kin": {
-     "name": "Babatune Lanre",
-     "relationship": "husband"
-     }
+    "name": "Asake Babatunde",
+    "age": 28,
+    "marital status": "Married",
+    "children": ["Ayo", "Tolu", "Simi"],
+    "staff": False,
+    "next of kin": {"name": "Babatune Lanre", "relationship": "husband"},
 }
 
-#subject python dict
+# subject python dict
 subject = {
-     "name": "Biology",
-     "teacher": {
-     "name": "Nana Ama",
-     "sex": "female"
-     },
-     "students_size": 24,
-     "elective": True,
-     "lesson days": ["Tuesday", "Friday"]
+    "name": "Biology",
+    "teacher": {"name": "Nana Ama", "sex": "female"},
+    "students_size": 24,
+    "elective": True,
+    "lesson days": ["Tuesday", "Friday"],
 }
 
 # reading json data
-with open('employee.json', 'r', encoding='UTF-8') as fp:
-    employee_object = json.load(fp)
+with open("employee.json", "r", encoding="utf-8") as file_handle:
+    employee_object = json.load(file_handle)
     print(employee_object)
 
 # encoding Python object to JSON and writing to file
-with open('mother.json', 'w', encoding='UTF-8') as fp:
-     json.dump(mother, fp, indent=1)
+with open("mother.json", "w", encoding="utf-8") as file_handle:
+    json.dump(mother, file_handle, indent=4)
 
 # using .dump()
-with open('subject.json', 'w', encoding='UTF-8') as fp:
-    json.dump(subject, fp, indent=3)
+with open("subject.json", "w", encoding="utf-8") as file_handle:
+    json.dump(subject, file_handle, indent=4)
 
 # using .dumps()
 json_data = json.dumps(subject)
@@ -45,11 +39,11 @@ print(json_data)
 print(type(json_data))
 
 
-#using .load()
-with open('students.json', 'r', encoding='UTF-8') as fp:
-     students_list = json.load(fp)
-     for student in students_list:
-          print(student)
+# using .load()
+with open("students.json", "r", encoding="utf-8") as file_handle:
+    students_list = json.load(file_handle)
+    for student in students_list:
+        print(student)
 
 
 # using .loads()
