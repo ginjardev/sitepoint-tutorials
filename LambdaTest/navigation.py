3273f592cce30e32a41a6f492a319b0e7e63ca15
ginjardev <arthurshur@gmail.com>
2024-02-02 23:09:25 +0000

    playwright

--- LambdaTest/navigation.py
+++ LambdaTest/navigation.py
@@ -1,41 +1,89 @@
-# import asyncio
+import asyncio
+from playwright.async_api import async_playwright, Playwright
 
-# from playwright.async_api import async_playwright, Playwright
+# wait for load state
+# async def run1(playwright: Playwright):
+#     chrome = playwright.chromium
+#     browser = await chrome.launch()
+#     context = await browser.new_context()
+#     page = await context.new_page()
+#     await page.goto("https://ecommerce-playground.lambdatest.io/")
+#     await page.wait_for_load_state()
+#     await page.screenshot(path="screenshot.png")
+
+#     print("Done")
+#     await browser.close()
 
 
 # async def main():
-#     async with async_playwright() as p:
-#         browser = await p.chromium.launch()
-#         page = await browser.new_page()
-#         await page.goto("https://playwright.dev/")  # Waits for load event
-#         # Now you can interact with elements on the page
-#         await page.screenshot(path="screenshot.png")
-#         html = await page.content()
-#         print(html)
+#     async with async_playwright() as playwright:
+#         await run1(playwright)
 
 
 # asyncio.run(main())
 
 
-import asyncio
-from playwright.async_api import async_playwright, Playwright
+# wait for url
+# async def run2(playwright: Playwright):
+#     chrome = playwright.chromium
+#     browser = await chrome.launch()
+#     page = await browser.new_page()
+#     await page.goto("https://ecommerce-playground.lambdatest.io/")
+#     await page.get_by_role("link", name="Blog", exact=True).click()
+#     await page.wait_for_url("**/blog/home")
+#     await page.screenshot(path="screenshot.png")
+#     await browser.close()
 
 
-async def run(playwright: Playwright):
+# async def main():
+#     async with async_playwright() as playwright:
+#         await run2(playwright)
+
+
+# asyncio.run(main())
+
+
+# wait for event
+
+# async def run3(playwright: Playwright):
+#     chrome = playwright.chromium
+#     browser = await chrome.launch()
+#     page = await browser.new_page()
+#     await page.goto(
+#         "https://ecommerce-playground.lambdatest.io/index.php?route=product/special"
+#     )
+#     await page.get_by_text("Continue", exact=True).click()
+#     await page.wait_for_event("domcontentloaded")
+#     await page.screenshot(path="screenshot.png")
+#     print("Done")
+#     await browser.close()
+
+
+# async def main():
+#     async with async_playwright() as playwright:
+#         await run3(playwright)
+
+
+# asyncio.run(main())
+
+# wait for function
+
+async def run3(playwright: Playwright):
     chrome = playwright.chromium
     browser = await chrome.launch()
-    context = await browser.new_context()
-    page = await context.new_page()
-    await page.goto("https://techchale.com")
+    page = await browser.new_page()
+    await page.goto("https://ecommerce-playground.lambdatest.io/")
+    # await page.get_by_text("Continue", exact=True).click()
+    buttons = await page.get_by_role("button")
+    await page.wait_for_event("domcontentloaded")
     await page.screenshot(path="screenshot.png")
-    html = await page.content()
-    print(html)
+    print("Done")
     await browser.close()
 
 
 async def main():
     async with async_playwright() as playwright:
-        await run(playwright)
+        await run3(playwright)
 
 
 asyncio.run(main())
